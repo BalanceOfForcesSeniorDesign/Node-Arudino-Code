17c85932bd73b7a039a99f44e9044711603d5f9f
krausnoah6504 <46875987+krausnoah6504@users.noreply.github.com>
2019-03-27 21:40:24 -0500

    Add files via upload

--- Nodes/Node_P/Node_P.ino
+++ Nodes/Node_P/Node_P.ino
@@ -31,6 +31,7 @@ int currentTime;
 // Sampling and FFT
 #define SAMPLES 128 // 128 * 4ms = .512 s
 #define SAMPLING_FREQUENCY 100 // 1/4ms = 250 Hz
+#define INTERVALS 2 // .512 s * 2 sampling intervals = last 1.024 s
 
 int pressureSamples[SAMPLES];
 double vPressureReal[SAMPLES];
@@ -51,6 +52,17 @@ struct payload_t {
   int nodeA_diff, nodeB_diff;
 };
 
+// Imbalance Decision variables
+#define forward_thresh = 50;
+#define backward_thresh = -50;
+int present_interval = 0;
+int previous_interval;
+int forward_count;
+int backward_count;
+int state[INTERVALS]; //State of walking or standing (0 or 1)
+int change[INTERVALS]; //Change from one state to another (-1 .. 2)
+int lean[INTERVALS]; // Direction of lean (-1..1)
+int imbalance[INTERVALS]; //Imbalance decision (0 or 1)
 
 void setupSensor()
 {
@@ -93,7 +105,7 @@ void setup(void)
 
 void loop(void) {
 
-
+    
   
     currentTime = micros();
 
@@ -136,45 +148,91 @@ void loop(void) {
       PressureFFT.ComplexToMagnitude();
       double domFrequency = PressureFFT.MajorPeak();
       Serial.println(domFrequency);
-      /*for(int i=1; i<(SAMPLES/2); i++)
-      {
-                  
-          Serial.print((i * 1.0 * SAMPLING_FREQUENCY) / SAMPLES, 1);
-          Serial.print(" ");
-          Serial.println(vPressureReal[i], 1);    //View only this line in serial plotter to visualize the bins
-      }*/
-
+      
+/* if (domFrequency > 1 || domFrequency < 3){ // If within frequency band
+ *  state[present_interval] = 0;  // State is walking
+ *  }
+ * else{
+ *  state[present_interval] = 1; // State is standing
+ *  }
+ */
     }
 
-    
-
-  
-    // Send message to PC reciever
-    /*Serial.println("Sending message....");
-    openRadioToNode(3, radio); // Switch to transmit to the PC node
-    payload_t packet = {ax, ay, az, gx, gy, gz, nodeA_diff, nodeB_diff};
-    if (radio.write(&packet, sizeof(packet)))
-    {
-      digitalWrite(13, HIGH);    // turn the LED on by making the voltage HIGH
+  /******************************Previous Interval Definition*************************************/
+    if (present_interval == 0){  //If present interval is first in interval loop
+      previous_interval = INTERVALS - 1; //position of last array element
     }
-    else
-    {
-      digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-    }*/
-
-/*
-    ax = a.acceleration.x;
-    ay = a.acceleration.y;
-    az = a.acceleration.z;
-    gx = g.gyro.x;
-    gy = g.gyro.y;
-    gz = g.gyro.z;
-
-    strip.setPixelColor(0, (int)abs(gx), (int)abs(gy), (int)abs(gz));
-    strip.show();
-*/
-    
-
-
-
+    else {
+      previous_interval = present_interval - 1; //previous position in array
+    }
+  /******************************Change Definition*************************************/
+    if (state[present_interval] + state[previous_interval] == 0){ // If 'walking' to 'walking'
+      change[present_interval] = -1; //walking
+      lean[present_interval] = 0;
+    }
+    else if (state[present_interval] + state[previous_interval] == 2){ // If 'standing' to 'standing'
+      change[present_interval] = 0; //standing
+    }
+    else if (state[present_interval] - state[previous_interval] == -1){ // If 'standing' to 'walking'
+      change[present_interval] = 1; //shifting
+    }
+    else if (state[present_interval] - state[previous_interval] == 1){ // If 'standing' to 'walking'
+      change[present_interval] = 2; //settling
+    } 
+    else { // something went wrong
+      change[present_interval] = 0; 
+    }
+  /******************************Lean Definition*************************************/  
+    if (change[present_interval] >= 0){ // If standing, shifting, or settling
+      forward_count = 0;
+      backward_count = 0;
+      for (int i = 0; i <= SAMPLES - 1; i++){ // for all samples
+        if (pressureSamples[i] > forward_thresh){ // if forward thresh crossed
+          forward_count++; // increment forward counter
+        }
+        else if (pressureSamples[i] < backward_thresh){ // if backward thresh crossed
+          backward_count++; // increment backward counter
+        }
+      }
+      if (forward_count > SAMPLES/2){ // if forward for more than half of samples
+        lean[present_interval] = 1; // Lean defined as forward
+      }
+      else if (backward_count > SAMPLES/2){ // if backward for more than half of samples
+        lean[present_interval] = -1;  // Lean defined as backward
+      }
+      else {
+        lean[present_interval] = 0; // Lean defined as neutral
+      }
+    }
+  /******************************Imbalance Definition*************************************/ 
+    if (change[present_interval] >= 0 && abs(lean[present_interval]) == 1){ // If [standing, shifting, or settilng] AND [leaning]
+      int threshgy = abs(g.gyro.y;);
+      int threshaz = abs(a.acceleration.z);
+      for (int i = 0; i <= SAMPLES; i++) {  
+        if (threshgy[i] >= 12 || threshaz[i] >= 4){
+          imbalance[present_interval] = 1;
+        }
+        else{
+          imbalance[present_interval] = 0;
+        }
+      }    
+    }
+    else{
+      imbalance[present_interval] = 0;
+    }    
+   // a.acceleration.z;
+   // g.gyro.y;
+  /******************************Display Results*************************************/ 
+  Serial.print(g.gyro.y);
+  Serial.print(" ");
+  Serial.print(a.acceleration.z);
+  Serial.print(" ");
+  Serial.print(imbalance[present_interval]);
+  /******************************Present Interval Definition*************************************/
+  if (present_interval < INTERVALS - 1){  // If length of array hasn't been reached
+    present_interval = present_interval++; // Increment interval
+  }
+  else{ // If length has been reached
+    present_interval = 0; //Loop back to first interval
+  }
 }
